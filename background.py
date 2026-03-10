4e4fdeaa39fa99c01436a8ec78a209213b11a099
beatrizbranfb <beatrizbfb@icloud.com>
2026-03-10 13:54:26 -0300

    defs

--- background.py
+++ background.py
@@ -1,8 +1,6 @@
-import os, dotenv
-from langchain_openai import OpenAiEmbeddings
-from langchain_openai import ChatOpenAI
-
-def load_api_key():
+def load_api_key(): #Puxar a Key do arquivo .env
+    import dotenv 
+    import os
     dotenv.load_dotenv()
     api_key = os.getenv("API_KEY")
     if not api_key:
@@ -10,10 +8,23 @@ def load_api_key():
     return api_key
 
 def agent():
-    ChatOpenAI(model="gpt-3.5-turbo",
-                   temperature = 0.2,
-                   api_key=load_api_key,
-                   System_prompt='''Você é um assistente virtual que tem como função ajudar os usuários a realizar fluxos dentro de um sistema chamado SisAmbientes, utilizado pelas recepcionistas do Tribunal de Contas da União.
+    from langchain.chat_models import ChatOpenAI
+    ChatOpenAI(model="gpt-3.5-turbo", #Modelo sendo utilizado
+            temperature = 0.2, #Temperatura é a "criatividade" do modelo. 
+            api_key=load_api_key, 
+            System_prompt='''Você é um assistente virtual que tem como função ajudar os usuários a realizar fluxos dentro de um sistema chamado SisAmbientes, utilizado pelas recepcionistas do Tribunal de Contas da União.
                 Você deve lembrar que não pode realizar o fluxo pelo usuário, não podendo realizar uma reserva, por exemplo.''',
-                    name = "SisAgente")
+            name = "SisAgente")
+    
+def RAG():
+    from langchain.document_loaders import PyPDFLoader #Leitor de PDF
+    from langchain.text_splitter import RecursiveCharacterTextSplitter #Divisor de texto
+    from langchain.embeddings import OpenAIEmbeddings #Embeddings é a transformação de texto em vetores numéricos
+    from langchain.vectorstores import chroma #DB vetorial
+    from langchain.chat_models import ChatOpenAI 
+    from langchain.chains import RetrievalQA #Recuperação de informação e geração de texto
+
+      
+
+
 
